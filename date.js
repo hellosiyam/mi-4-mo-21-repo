0385785f7f824a3c74e0dc6c3cfa902cdaf1b80a
hellosiyam <hellosiyambd@gmail.com>
2025-01-04 23:05:45 +0600

    mouduile 21

---
+++ date.js
@@ -0,0 +1,7 @@
+const day = new Date("2029-10-19");
+console.log(day.getMonth());
+console.log(day.getDay());
+const specificDate = new Date(2091, 0, 10);
+console.log(specificDate);
+
+
---
+++ swap.js
@@ -0,0 +1,18 @@
+let a = 10;
+let b = 15;
+console.log(a, b);
+
+const tem = a;
+a = b;
+b = tem;
+console.log(a,b);
+
+// -----------------------------------
+let x = 5;
+let y = 8;
+console.log(x,y);
+
+[x,y]=[y,x];
+console.log(x,y);
+
+
