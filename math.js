f8bdae2885868e2e542f47a01201801341edd367
hellosiyam <hellosiyambd@gmail.com>
2025-01-04 22:35:14 +0600

    mouduile 21

---
+++ math.js
@@ -0,0 +1,20 @@
+const sum = Math.min(26, 121, 21, 2, 13, 1, )
+console.log(sum);
+const add = Math.max(1500, 1255, 3215, 2250, 1852, 1235, 15361)
+console.log(add);
+console.log(Math.PI);
+console.log(Math.abs(100-20));
+console.log(Math.round(2.4));
+console.log(Math.ceil(5.5));
+console.log(Math.floor(3.5));
+console.log(Math.random());
+console.log(Math.random()*10);
+const ren =Math.random(Math.random()*10);
+console.log(ren);
+
+
+
+
+
+
+
